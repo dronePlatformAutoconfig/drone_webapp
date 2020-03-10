b3180f7105072f9ceaaf0492318fff2e2ff49cab
egoros <egoros97@yandex.ru>
2020-03-10 13:46:58 +0300

    created develop branch

--- drone-ui/src/app/app.module.ts
+++ drone-ui/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PageComponent } from './page/page.component';
+import { HomeComponent } from './home/home.component';
+import { StationComponent } from './station/station.component';
+import { DroneComponent } from './drone/drone.component';
+import { ActionsComponent } from './actions/actions.component';
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageComponent
+    HomeComponent,
+    StationComponent,
+    DroneComponent,
+    ActionsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
