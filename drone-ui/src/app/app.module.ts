752020ca39106b5b9479c377c25d53af1da6ad09
egoros <egoros97@yandex.ru>
2020-03-22 19:19:49 +0300

    basic subscriber components

--- drone-ui/src/app/app.module.ts
+++ drone-ui/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { FormsModule } from "@angular/forms";
 import { ConnectionComponent } from './connection/connection.component';
 import { ChargeComponent } from './charge/charge.component';
 import { SubscriberComponent } from './subscriber/subscriber.component';
+import { CenterComponent } from './center/center.component';
+import { RoofComponent } from './roof/roof.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +25,8 @@ import { SubscriberComponent } from './subscriber/subscriber.component';
     ConnectionComponent,
     ChargeComponent,
     SubscriberComponent,
+    CenterComponent,
+    RoofComponent,
   ],
   imports: [
     BrowserModule,
