c683a444b2693d8defe812a40045a4a4e7197d09
egoros <egoros97@yandex.ru>
2020-03-22 16:18:41 +0300

    added subscriber

---
+++ drone-ui/src/app/subscriber/subscriber.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import * as ROSLIB from 'roslib';
+import {ROSConnectionService} from '../rosconnection.service';
+
+@Component({
+  selector: 'app-subscriber',
+  templateUrl: './subscriber.component.html',
+  styleUrls: ['./subscriber.component.scss']
+})
+export class SubscriberComponent implements OnInit {
+
+  constructor(private rosConnection: ROSConnectionService) {
+  }
+
+  topic = '';
+  messageType = '';
+  message = {};
+
+  ngOnInit(): void {
+    document.addEventListener('rosconnected', () => {
+      console.log(this.topic + ' topic');
+      this.startListener();
+    });
+  }
+  startListener() {
+    const listener = new ROSLIB.Topic({
+      ros : this.rosConnection.ros,
+      name : this.topic,
+      messageType : this.messageType
+    });
+    listener.subscribe((message) => {
+        this.message = message;
+        console.log(message);
+      }
+    );
+  }
+}
