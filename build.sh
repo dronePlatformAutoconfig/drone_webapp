6e28c309a90b0e16ad63b9a206cb5a463dba4c65
EgorOs <egoros97@yandex.ru>
2021-05-09 02:56:12 +0300

    Added travis ci

--- build.sh
+++ build.sh
@@ -7,3 +7,7 @@ cd -
 rm -rf $PROD_DIST_PATH
 mkdir -p $PROD_DIST_PATH
 cp -r drone-ui/dist/DroneInterface/* $PROD_DIST_PATH
+
+rm app.zip
+cd server/; zip -r -D app.zip * -x node_modules/**\*; mv app.zip ..; cd -
+
